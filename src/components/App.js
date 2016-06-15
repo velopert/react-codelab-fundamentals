09ec7a13dc2ed04c16045f5191bf826a79bef698
velopert <public.velopert@gmail.com>
2016-06-15 23:35:28 +0900

    .

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <h1>Welcome to CodeLab!!</h1>
+        );
+    }
+}
+
+export default App;
