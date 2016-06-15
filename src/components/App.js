eb1537cda1989d9887db15c392fa02bf497e2cb2
velopert <public.velopert@gmail.com>
2016-06-16 00:12:32 +0900

    .

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render(){
+
+        return (
+                <h1>Hello React Skeleton</h1>
+        );
+    }
+}
+
+export default App;
