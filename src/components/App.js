66ba2b132be9466ecbc086d0ba3485d62f245857
velopert <public.velopert@gmail.com>
2016-08-26 16:52:32 +0900

    copied codes from codepen

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import Contact from './Contact';
 
 class App extends React.Component {
     render(){
-
         return (
-                <h1>Hello React Skeleton</h1>
+                <Contact/>
         );
     }
 }
