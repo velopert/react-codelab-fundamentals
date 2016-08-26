78ae6f11268de02b5ff48badb3b20b13191a1490
velopert <public.velopert@gmail.com>
2016-08-26 21:08:51 +0900

    using update, handleCreate,Edit,Remove, implement onCreate'

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import ContactCreate from './ContactCreate';
 
+import update from 'react-addons-update';
 
 export default class Contact extends React.Component {
 
@@ -26,6 +28,11 @@ export default class Contact extends React.Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
     }
 
     handleChange(e) {
@@ -39,6 +46,39 @@ export default class Contact extends React.Component {
             selectedKey: key
         });
         console.log(key, 'is selected');
+    }
+
+    handleCreate(contact) {
+        this.setState({
+            contactData: update(
+                this.state.contactData,
+                { $push: [contact] }
+            )
+        });
+    }
+
+
+    handleRemove() {
+        this.setState({
+            contactData: update(
+                this.state.contactData,
+                { $splice: [[this.state.selectedKey, 1]] }
+            )
+        });
+    }
+
+    handleEdit(name, phone) {
+        this.setState({
+            contactData: update(
+                this.state.contactData,
+                {
+                    [this.state.selectedKey]: {
+                        name: { $set: name },
+                        phone: { $set: phone }
+                    }
+                }
+            )
+        });
     }
 
     render() {
@@ -66,13 +106,15 @@ export default class Contact extends React.Component {
                     name="keyword"
                     placeholder="Search"
                     value={this.state.keyword}
-                    onChange={this.handleChange}
-                />
+                    onChange={this.handleChange}/>
                 <div>{mapToComponents(this.state.contactData)}</div>
                 <ContactDetails
                     contact={this.state.contactData[this.state.selectedKey]}
-                    isSelected={this.state.selectedKey!=-1}
-                />
+                    isSelected={this.state.selectedKey!=-1}/>
+                <ContactCreate onCreate={this.handleCreate}/>
+
+
+
             </div>
         );
     }
