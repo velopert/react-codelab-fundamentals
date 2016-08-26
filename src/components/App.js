7ae4cef06c5c7eea8a9466e801290756ad78f771
velopert <public.velopert@gmail.com>
2016-08-27 00:02:03 +0900

    mapToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Counter from './Counter';
 import Controls from './Controls';
+
+import * as actions from 'actions';
+
+import { connect } from 'react-redux';
+
 class App extends React.Component {
     render(){
 
@@ -13,4 +18,25 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        value: state.counter.value,
+        config: {
+            diff: state.counter.diff,
+            visibility: state.uistate.visibility
+        }
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSetDiff: (value) => dispatch(actions.setDiff(value)),
+        onIncrease: () => dispatch(actions.increase()),
+        onDecrease: () => dispatch(actions.decrease()),
+        onToggleVisibility: () => dispatch(actions.toggleControlVisibility())
+    };
+};
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
